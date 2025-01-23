1b9fef70976e405bf4678ddfe1bbcb1dc62691f1
yuyuaqwq <1454832774@qq.com>
2025-01-23 18:21:16 +0800

    常量池优化

--- src/const_pool.cpp
+++ src/const_pool.cpp
@@ -8,12 +8,17 @@ uint32_t ConstPool::New(const Value& value) {
 }
 
 uint32_t ConstPool::New(Value&& value) {
+	auto lock = std::lock_guard(mutex_);
 	uint32_t const_idx;
 	auto it = const_map_.find(value);
 	if (it == const_map_.end()) {
-		const_idx = pool_.size();
-		pool_.emplace_back(std::move(value));
+		if (const_index_ % kStaticArraySize == 0) {
+			auto i1 = const_index_ / kStaticArraySize;
+			pool_[i1] = std::make_unique<StaticArray>();
+		}
+		const_idx = const_index_++;
 		const_map_.emplace(value, const_idx);
+		Get(const_idx) = std::move(value);
 	}
 	else {
 		const_idx = it->second;
@@ -21,18 +26,14 @@ uint32_t ConstPool::New(Value&& value) {
 	return const_idx;
 }
 
-// 负数表示从尾部索引起
-const Value& ConstPool::Get(int32_t index) const {
-	if (index >= 0) {
-		return pool_[index];
-	}
-	else {
-		return pool_[pool_.size() + index];
-	}
+const Value& ConstPool::Get(uint32_t index) const {
+	return const_cast<ConstPool*>(this)->Get(index);
 }
 
-size_t ConstPool::Size() const noexcept {
-	return pool_.size();
+Value& ConstPool::Get(uint32_t index) {
+	auto i1 = index / kStaticArraySize;
+	auto i2 = index % kStaticArraySize;
+	return (*pool_[i1])[i2];
 }
 
 } // namespace mjs
--- src/const_pool.h
+++ src/const_pool.h
@@ -1,30 +1,35 @@
 #pragma once
 
 #include <vector>
+#include <array>
 #include <string>
 #include <memory>
+#include <mutex>
 
 #include "value.h"
 
 namespace mjs {
 
-// 需要注意的是，pool应该修改为使用分段静态数组，避免resize使得其他运行Context的线程访问const
-// 如每块静态数组有1024个元素，满了之后就new一块新静态数组，放回动态数组中
-// 索引访问则是idx / 1024找到动态数组索引，idx % 1024找到静态数组索引
+// pool使用分段静态数组，避免resize使得其他运行Context的线程访问const
+// 如每块静态数组有1024个元素，2级数组满了之后就new一块新静态数组，放回1级数组中
+// 索引访问则是idx / 1024找到1级数组索引，idx % 1024找到2级数组索引
 class ConstPool {
+private:
+	static constexpr size_t kStaticArraySize = 1024;
+	using StaticArray = std::array<Value, kStaticArraySize>;
+
 public:
 	uint32_t New(const Value& value);
 	uint32_t New(Value&& value);
 
-	// 负数表示从尾部索引起
-	const Value& Get(int32_t index) const;
-	size_t Size() const noexcept;
+	const Value& Get(uint32_t index) const;
+	Value& Get(uint32_t index);
 
 private:
-	// 常量
+	std::mutex mutex_;
 	std::map<Value, uint32_t> const_map_;
-
-	std::vector<Value> pool_;
+	std::array<std::unique_ptr<StaticArray>, kStaticArraySize> pool_;
+	uint32_t const_index_ = 0;
 };
 
 } // namespace mjs
